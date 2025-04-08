d7ca0e0047c95e8e9058cbc388c323b44467aa1e
Divyesh <divyeshjoshi1106@gmail.com>
2025-04-08 15:25:44 +0200

    entities files updated, constructor, getter and setters added

--- app/src/main/java/ticket/booking/App.java
+++ app/src/main/java/ticket/booking/App.java
@@ -3,6 +3,12 @@
  */
 package ticket.booking;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 public class App {
 
     public static void main(String[] args) {
--- app/src/main/java/ticket/booking/entities/Train.java
+++ app/src/main/java/ticket/booking/entities/Train.java
@@ -1,6 +1,5 @@
 package ticket.booking.entities;
 
-import java.sql.Time;
 import java.util.List;
 import java.util.Map;
 
@@ -8,6 +7,58 @@ public class Train {
     private String trainID;
     private String trainNo;
     private List<List<Integer>> seats;
-    private Map<String, Time> stationTimes;
+    private Map<String, String> stationTimes;
     private List<String> stations;
+
+    public Train(String trainID, String trainNo, List<List<Integer>> seats, Map<String, String> stationTimes, List<String> stations) {
+        this.trainID = trainID;
+        this.trainNo = trainNo;
+        this.seats = seats;
+        this.stationTimes = stationTimes;
+        this.stations = stations;
+    }
+
+    public String getTrainID() {
+        return trainID;
+    }
+
+    public void setTrainID(String trainID) {
+        this.trainID = trainID;
+    }
+
+    public String getTrainNo() {
+        return trainNo;
+    }
+
+    public void setTrainNo(String trainNo) {
+        this.trainNo = trainNo;
+    }
+
+    public List<List<Integer>> getSeats() {
+        return seats;
+    }
+
+    public void setSeats(List<List<Integer>> seats) {
+        this.seats = seats;
+    }
+
+    public Map<String, String> getStationTimes() {
+        return stationTimes;
+    }
+
+    public void setStationTimes(Map<String, String> stationTimes) {
+        this.stationTimes = stationTimes;
+    }
+
+    public List<String> getStations() {
+        return stations;
+    }
+
+    public void setStations(List<String> stations) {
+        this.stations = stations;
+    }
+
+    public String getTrainInfo(){
+        return String.format("Train ID: %s Train No: %s", trainID, trainNo);
+    }
 }
